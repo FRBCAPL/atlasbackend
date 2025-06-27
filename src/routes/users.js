c8b821f53a96db388b7bd176ebb10fb7b211a879
frbcapl@gmail.com <frbcapl@gmail.com>
2025-06-26 23:09:08 -0600

    Add missing admin routes for dashboard functionality

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/:idOrEmail', userController.getUser);
+router.get('/', userController.getAllUsers);
 router.post('/admin/sync-users', userController.syncUsers);
+router.get('/:idOrEmail', userController.getUser);
 
 module.exports = router; 
